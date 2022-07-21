980fd1883a9281f8680a50c75b712ab67706602a
JessAT18 <j-aquino@outlook.com>
2022-07-21 15:11:13 -0400

    Persistencia de datos con Local Storage

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { AppUI } from "./AppUI";
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el curso de intro a React', completed: false },
-  { text: 'Llorar con la llorona', completed: false },
-];
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el curso de intro a React', completed: false },
+//   { text: 'Llorar con la llorona', completed: false },
+// ];
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -26,11 +36,17 @@ function App() {
     });
   }
 
+  const saveTodos = (newTodos) => {
+    const stringyfiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringyfiedTodos);
+    setTodos(newTodos);
+  };
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
     // todos[todoIndex] = {
     //   text: todos[todoIndex].text,
     //   completed: true
@@ -42,7 +58,7 @@ function App() {
     const newTodos = [...todos];
     //Funcion para eliminar desde la posicion indicada la cantidad de elementos que se desean eliminar.
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
