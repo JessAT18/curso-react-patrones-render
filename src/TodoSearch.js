83ac8eec79c0f87d6e33e7023476ec4a5a5ccb4a
JessAT18 <j-aquino@outlook.com>
2022-07-21 12:11:35 -0400

    Manejo del estado

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState('');
+
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
+    setSearchValue(event.target.value);
   };
-  return(
+  return[
     <input 
       className="TodoSearch"
       placeholder="Salvar el semestre en 3 dias y contando..."
+      value = {searchValue}
       onChange={onSearchValueChange}
-    />
-  );
+    />,
+    <p>{searchValue}</p>
+  ];
 }
 
 export { TodoSearch };
