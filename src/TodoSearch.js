0eb899584de8553ebc50b184a1c6202f3f6e0c3b
JessAT18 <j-aquino@outlook.com>
2022-07-21 13:52:41 -0400

    Contando y buscando todos

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
-  const [searchValue, setSearchValue] = React.useState('');
-
+function TodoSearch({ searchValue, setSearchValue }) {
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
     setSearchValue(event.target.value);
   };
-  return[
+  return(
     <input 
       className="TodoSearch"
       placeholder="Salvar el semestre en 3 dias y contando..."
       value = {searchValue}
       onChange={onSearchValueChange}
-    />,
-    <p>{searchValue}</p>
-  ];
+    />
+  );
 }
 
 export { TodoSearch };
