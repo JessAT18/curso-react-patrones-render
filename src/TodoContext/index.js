4e32b0b7f8edf7354d9db6f9694d9bad44c7d5a4
JessAT18 <j-aquino@outlook.com>
2022-08-01 11:33:03 -0400

    Concluyendo formulario para crear TODOs

--- src/TodoContext/index.js
+++ src/TodoContext/index.js
@@ -4,78 +4,92 @@ import { useLocalStorage } from './useLocalStorage';
 const TodoContext = React.createContext();
 
 function TodoProvider(props) {
-    const {
-        item: todos, 
-        saveItem: saveTodos,
-        loading,
-        error
-      } = useLocalStorage('TODOS_V1', []);
-      const [searchValue, setSearchValue] = React.useState('');
-      const [openModal, setOpenModal] = React.useState(false);
-    
-      const completedTodos = todos.filter(todo => !!todo.completed).length;
-      const totalTodos = todos.length;
-    
-      let searchedTodos = [];
-    
-      if (!searchValue.length >= 1) {
-        searchedTodos = todos;
-      } else {
-        searchedTodos = todos.filter(todo => {
-          const todoText = todo.text.toLowerCase();
-          const searchText = searchValue.toLowerCase();
-          return todoText.includes(searchText);
-        });
-      }
-    
-      const completeTodo = (text) => {
-        const todoIndex = todos.findIndex(todo => todo.text === text);
-        const newTodos = [...todos];
-        newTodos[todoIndex].completed = true;
-        saveTodos(newTodos);
-        // todos[todoIndex] = {
-        //   text: todos[todoIndex].text,
-        //   completed: true
-        // };
-      };
-    
-      const deleteTodo = (text) => {
-        const todoIndex = todos.findIndex(todo => todo.text === text);
-        const newTodos = [...todos];
-        //Funcion para eliminar desde la posicion indicada la cantidad de elementos que se desean eliminar.
-        newTodos.splice(todoIndex, 1);
-        saveTodos(newTodos);
-      };
-    
-      // console.log('Render antes del use effect');
-    
-      // React.useEffect(() => {
-      //   //Justo antes de hacer el render se ejecutan los efectos
-      //   console.log('Use effect');
-      // }, 
-      // [totalTodos]  //Si borramos un todo o creamos uno nuevo, deberia ejecutarse el useEffect
-      // ); //[] Definir cuando ejecutar este use effect, si le mandamos un arreglo vacio se ejecutará solo una vez
-    
-      // console.log('Luego del use effect');
-    
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error
+  } = useLocalStorage('TODOS_V1', []);
+  const [searchValue, setSearchValue] = React.useState('');
+  const [openModal, setOpenModal] = React.useState(false);
 
-    return (
-        <TodoContext.Provider value={{
-            loading,
-            error,
-            totalTodos,
-            completedTodos,
-            searchValue,
-            setSearchValue,
-            searchedTodos,
-            completeTodo,
-            deleteTodo,
-            openModal,
-            setOpenModal
-        }}>
-            {props.children}
-        </TodoContext.Provider>
-    );
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
+    saveTodos(newTodos);
+    // todos[todoIndex] = {
+    //   text: todos[todoIndex].text,
+    //   completed: true
+    // };
+  };
+
+  const addTodo = (text) => {
+    const newTodos = [...todos];
+    newTodos.push({
+      completed: false,
+      text,
+    });
+    saveTodos(newTodos);
+    // todos[todoIndex] = {
+    //   text: todos[todoIndex].text,
+    //   completed: true
+    // };
+  };
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    //Funcion para eliminar desde la posicion indicada la cantidad de elementos que se desean eliminar.
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  };
+
+  // console.log('Render antes del use effect');
+
+  // React.useEffect(() => {
+  //   //Justo antes de hacer el render se ejecutan los efectos
+  //   console.log('Use effect');
+  // }, 
+  // [totalTodos]  //Si borramos un todo o creamos uno nuevo, deberia ejecutarse el useEffect
+  // ); //[] Definir cuando ejecutar este use effect, si le mandamos un arreglo vacio se ejecutará solo una vez
+
+  // console.log('Luego del use effect');
+
+
+  return (
+    <TodoContext.Provider value={{
+      loading,
+      error,
+      totalTodos,
+      completedTodos,
+      searchValue,
+      setSearchValue,
+      searchedTodos,
+      completeTodo,
+      addTodo,
+      deleteTodo,
+      openModal,
+      setOpenModal
+    }}>
+      {props.children}
+    </TodoContext.Provider>
+  );
 }
 
 export { TodoContext, TodoProvider };
