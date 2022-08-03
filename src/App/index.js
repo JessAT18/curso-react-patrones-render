4e77c4026fede1e5b9a511bcf471f852fd1c11b2
JessAT18 <j-aquino@outlook.com>
2022-08-03 14:29:49 -0400

    Poniendo en práctica las render props

--- src/App/index.js
+++ src/App/index.js
@@ -31,12 +31,14 @@ function App() {
         />
       </TodoHeader>
 
-      <TodoList>
-        {error && <TodosError/>}
-        {loading && <TodosLoading/>}
-        {(!loading && !searchedTodos.length) && <EmptyTodos/>}
-
-        {searchedTodos.map(todo => (
+      <TodoList
+        error={error}
+        loading={loading}
+        searchedTodos={searchedTodos}
+        onError={() => <TodosError/>}
+        onLoading={() => <TodosLoading/>}
+        onEmptyTodos={() => <EmptyTodos/>}
+        render={todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
@@ -44,9 +46,10 @@ function App() {
             onComplete={() => completeTodo(todo.text)}
             onDelete={() => deleteTodo(todo.text)}
           />
-        ))}
-      </TodoList>
+        )}
 
+      />
+      
       {!!openModal && (
         <Modal>
           <TodoForm
--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,9 +1,16 @@
-import react from "react";
+import React from "react";
 import './TodoList.css';
 
 function TodoList(props){
     return(
-        <section>
+        <section className="TodoList-container">
+            {props.error && props.onError()}
+            {props.loading && props.onLoading()}
+            {(!props.loading && !props.searchedTodos?.length) && props.onEmptyTodos()}
+            
+            {props.searchedTodos.map(props.render)}
+            {/* todo => props.render(todo) */}
+
             <ul>
                 {props.children}
             </ul>
