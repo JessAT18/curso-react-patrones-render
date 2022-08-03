dad5c6ad7b8f8649ad7ce07f0ac10df7e5f3d06a
JessAT18 <j-aquino@outlook.com>
2022-08-03 16:21:54 -0400

    Creando un HOC 💀

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+
+function App(props) {
+  return (
+    <h1>{props.saludo}, {props.nombre}!</h1>
+  );
+}
+
+// function withWhatever(WrappedComponent) {
+//   return function ComponenteDeVerdad(props) {
+//     return (
+//       <React.Fragment>
+//         <WrappedComponent {...props} />
+//         <p>No entiendo nada :(</p>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+function withSaludo(WrappedComponent) { //function withSaludo(WrappedComponent) 
+  return function WrappedComponentWithSaludo(saludo) {//function WrappedComponentWithSaludo(saludo)
+    return function ComponenteDeVerdad(props) {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} saludo={saludo} />
+          <p>No entiendo nada :( 💀</p>
+        </React.Fragment>
+      );
+    }
+  }
+}
+
+// const AppWithWhatever = withWhatever(App);
+const AppWithSaludo = withSaludo(App)("Buenas");
+//const AppWithSaludo = withSaludo("Buenas")(App);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  // <App saludo="Buenas" nombre="Jess"/>
+  // <AppWithWhatever saludo="Hey" nombre="Jess"/>
+  <AppWithSaludo nombre="Jess"/>
 );
