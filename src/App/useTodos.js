684c2aad4fe88eb0621029d59271b53fdcbb64d1
JessAT18 <j-aquino@outlook.com>
2022-08-03 13:54:37 -0400

    Composición de componentes sin React Context

--- src/App/useTodos.js
+++ src/App/useTodos.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
-const TodoContext = React.createContext();
-
-function TodoProvider(props) {
+function useTodos() {
   const {
     item: todos,
     saveItem: saveTodos,
@@ -71,25 +69,20 @@ function TodoProvider(props) {
 
   // console.log('Luego del use effect');
 
-
-  return (
-    <TodoContext.Provider value={{
-      loading,
-      error,
-      totalTodos,
-      completedTodos,
-      searchValue,
-      setSearchValue,
-      searchedTodos,
-      completeTodo,
-      addTodo,
-      deleteTodo,
-      openModal,
-      setOpenModal
-    }}>
-      {props.children}
-    </TodoContext.Provider>
-  );
+  return {
+    loading,
+    error,
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setSearchValue,
+    searchedTodos,
+    completeTodo,
+    addTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal
+  };
 }
 
-export { TodoContext, TodoProvider };
+export { useTodos };
